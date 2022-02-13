a2016b4a99939187e0feaae125cf9a5741420ce8
Claudia Lisboa <ccalumby@gmail.com>
2022-02-12 19:16:50 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const colors =["green","yellow","black","violet","wheat","purple" ,"red","orange","blue"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+    //get random number between 0-3 colors[]
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
